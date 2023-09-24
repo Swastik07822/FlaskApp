18039af50d876e01dcabde970e0a1c3f6044dd77
Swastik07822 <116582848+Swastik07822@users.noreply.github.com>
2023-09-24 19:30:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import requests
 from customize import customize
 
 def get_weather(place):
-    i = requests.get("http://api.weatherstack.com/current?access_key=ccf2d3b290bd33c9be211b4605cb254d&query="+place+"'").json()
+    i = requests.get("http://api.weatherstack.com/current?access_key=&query="+place+"'").json()
     i,j,k = customize(i)
     
     return i,j,k
